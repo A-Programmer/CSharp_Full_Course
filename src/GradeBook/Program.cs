dda57dc4ca2a0a4657a6ffbab001bcb050981a41
Kamran Sadin <mrsadin@gmail.com>
2021-09-20 08:18:57 +0430

    02. Learning the C# Syntax

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() {85.67, 89, 90, 79.25};
+            grades.Add(97);
+            grades.Add(68);
+
+            double result = 0;
+            foreach(var grade in grades)
+            {
+                result += grade;
+            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The average is {result:N2}");
+        }
+    }
+}
+
+//if
+//list
+//array
+//var
+//foreach
