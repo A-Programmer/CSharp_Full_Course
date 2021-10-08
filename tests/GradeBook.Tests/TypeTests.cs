965c9c3354c6cb376d8c37616c5671c2e3dc66d9
Kamran Sadin <mrsadin@gmail.com>
2021-10-08 12:43:20 +0330

    Writing Unit Test called CanSetNameFromReference

--- tests/GradeBook.Tests/TypeTests.cs
+++ tests/GradeBook.Tests/TypeTests.cs
@@ -3,73 +3,8 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-
-
     public class TypeTests
     {
-
-        [Fact]
-        public void StringBehavesLikeValueTypes()
-        {
-            string name = "Kamran";
-            var upper = MakeUpperCase(name);
-
-            Assert.Equal("Kamran", name);
-            Assert.Equal("KAMRAN", upper);
-        }
-
-        private string MakeUpperCase(string parameter)
-        {
-            return parameter.ToUpper();
-        }
-
-        [Fact]
-        public void Test1()
-        {
-            var x = GetInt();
-            SetInt(ref x);
-            
-            Assert.Equal(3, x);
-        }
-
-        private void SetInt(ref int z)
-        {
-            z = 42;
-            z = 3;
-        }
-
-        private int GetInt()
-        {
-            return 3;
-        }
-
-        [Fact]
-        public void CSharpCanPassByRef()
-        {
-            var book1 = GetBook("Book 1");
-            GetBookSetName(ref book1, "New Name");
-
-            Assert.Equal("New Name", book1.Name);
-        }
-
-        private void GetBookSetName(ref Book book, string name)
-        {
-            book = new Book(name);
-        }
-
-        [Fact]
-        public void CSharpIsPassByValue()
-        {
-            var book1 = GetBook("Book 1");
-            GetBookSetName(book1, "New Name");
-
-            Assert.Equal("Book 1", book1.Name);
-        }
-
-        private void GetBookSetName(Book book, string name)
-        {
-            book = new Book(name);
-        }
 
         [Fact]
         public void CanSetNameFromReference()
