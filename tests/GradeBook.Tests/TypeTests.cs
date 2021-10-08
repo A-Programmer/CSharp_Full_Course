3a9439c2bb6c358d7c54f1f395c1520a41f8716e
Kamran Sadin <mrsadin@gmail.com>
2021-10-08 12:44:16 +0330

    Writing Unit Test called CSharpCanPassByRef

--- tests/GradeBook.Tests/TypeTests.cs
+++ tests/GradeBook.Tests/TypeTests.cs
@@ -7,6 +7,20 @@ namespace GradeBook.Tests
     {
 
         [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
         public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
