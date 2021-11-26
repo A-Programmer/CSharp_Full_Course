16f65bf75b0e24eb9683eec700d3276ea5a0f513
Kamran Sadin <mrsadin@gmail.com>
2021-11-26 20:36:26 +0330

    Working with Arrays

--- 02. Beginning CSharp Collections/NthDay/Program.cs
+++ 02. Beginning CSharp Collections/NthDay/Program.cs
@@ -6,7 +6,28 @@ namespace NthDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "----",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.WriteLine("\nBefore:");
+            foreach (var item in daysOfWeek)
+                Console.WriteLine(item);
+
+            daysOfWeek[2] = "Wednesday";
+
+            Console.WriteLine("\n\nAfter:");
+            foreach (var item in daysOfWeek)
+                Console.WriteLine(item);
+
+
         }
     }
 }
