8a5ea894a01050aa003e2bb421fcee37e7b265d3
Kamran Sadin <mrsadin@gmail.com>
2021-11-27 11:51:37 +0330

    2nd Project of Collections in C# Course added

---
+++ 02. Beginning CSharp Collections/TopTenPops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
