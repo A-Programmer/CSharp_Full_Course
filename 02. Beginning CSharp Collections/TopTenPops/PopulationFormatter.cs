b3dbbef2ffdf35457ce9fc6364469432e48c7ede
Kamran Sadin <mrsadin@gmail.com>
2021-11-29 10:27:21 +0330

    New helper class called PopulationFormatter added to project

--- 02. Beginning CSharp Collections/TopTenPops/PopulationFormatter.cs
+++ 02. Beginning CSharp Collections/TopTenPops/PopulationFormatter.cs
@@ -3,28 +3,55 @@ namespace TopTenPops
 {
     public class PopulationFormatter
     {
-        //public static string FormatPopulation(int population)
-        //{
-        //    if (population == 0)
-        //        return "(Unknown)";
-
-        //    int popRounded = RoundPopulation4(population);
-
-        //    return $"{popRounded: ### ### ### ###}".Trim();
-        //}
-
-
-        ////Rounds the population
-        //private static int RoundPopulation4(int population)
-        //{
-        //    int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_0001), 1);
-
-        //    return RoundToNearest(population, accuracy);
-        //}
-
-
-        public PopulationFormatter()
+        public static string FormatPopulation(int population)
         {
+            if (population == 0)
+                return "(Unknown)";
+
+            int popRounded = RoundPopulation4(population);
+
+            return $"{popRounded:### ### ### ###}".Trim();
+        }
+
+        // Rounds the population to 4 significant figures
+        private static int RoundPopulation4(int population)
+        {
+            // work out what rounding accuracy we need if we are to round to 
+            // 4 significant figures
+            int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_000L), 1);
+
+            // do the rounding
+            return RoundToNearest(population, accuracy);
+
+        }
+
+        /// Rounds the number to the specified accuracy
+        /// For example, if the accuracy is 10, then we round to the nearest 10:
+        /// 23 -> 20
+        /// 25 -> 30
+        /// etc.
+        public static int RoundToNearest(int exact, int accuracy)
+        {
+            int adjusted = exact + accuracy / 2;
+            return adjusted - adjusted % accuracy;
+        }
+
+        /// Returns the highest number that is a power of 10 and is no larger than the number supplied
+        /// Examples:
+        /// GetHighestPowerOfTen(11) = 10
+        /// GetHighestPowerOfTen(99) = 10
+        /// GetHighestPowerOfTen(100) = 100
+        /// GetHighestPowerOfTen(843) = 100
+        /// GetHighestPowerOfTen(1000) = 1000
+        public static long GetHighestPowerofTen(int x)
+        {
+            long result = 1;
+            while (x > 0)
+            {
+                x /= 10;
+                result *= 10;
+            }
+            return result;
         }
     }
 }
